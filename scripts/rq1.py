00bd884f02061c7281386c87f0c09bfe8dcc50eb
Ian Guelman <ianguelman@gmail.com>
2022-02-16 13:43:37 -0300

    Feat: Query unica

--- scripts/rq1.py
+++ scripts/rq1.py
@@ -1,3 +1,4 @@
+from math import ceil
 import os
 from unittest import result
 from utils.graphql import GraphQL
@@ -9,30 +10,9 @@ PER_PAGE = 100
 def run():
     nodes = []
     graphql = GraphQL(os.environ["API_URL"])
+    lastCursor = None
 
-    response = graphql.post(
-        """
-        query popularRepositories($perPage: Int) {
-            search(query: "stars:>100", type: REPOSITORY, first: $perPage) {
-                nodes {
-                ... on Repository {
-                        nameWithOwner
-                        url
-                        createdAt
-                    }
-                }
-                pageInfo {
-                    endCursor
-                    hasNextPage
-                }
-            }    
-        }
-        """, {"perPage": PER_PAGE}
-    )
-
-    nodes = nodes + response["data"]["search"]["nodes"]
-
-    for x in range(1, int(TOTAL_ITEMS/PER_PAGE)):
+    for x in range(0, ceil(TOTAL_ITEMS/PER_PAGE)):
         response = graphql.post(
             """
             query ($lastCursor: String, $perPage: Int) {
@@ -50,10 +30,17 @@ def run():
                     }
                 }    
             }
-            """, {"lastCursor": response["data"]["search"]["pageInfo"]["endCursor"],
-                  "perPage": PER_PAGE}
+            """, {"lastCursor": lastCursor,
+                  "perPage": min(PER_PAGE, TOTAL_ITEMS, (TOTAL_ITEMS - len(nodes)))}
         )
+
         print(response)
+
+        if not response["data"]["search"]["pageInfo"]["hasNextPage"]:
+            break
+
+        lastCursor = response["data"]["search"]["pageInfo"]["endCursor"]
+
         nodes = nodes + response["data"]["search"]["nodes"]
 
     print(len(nodes))
